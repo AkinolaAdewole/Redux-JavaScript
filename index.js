c58290f4049ecf84118cba6fc872ad462c91929f
Akinola <adewoleakinola24@gmail.com>
2023-07-13 21:03:37 +0100

    JavaScript with Redux

--- index.js
+++ index.js
@@ -4,7 +4,12 @@
 const redux = require('redux');
 const createStore=redux.createStore;
 const bindActionCreators= redux.bindActionCreators;
-const combineReducers=redux.combineReducers
+const combineReducers=redux.combineReducers;
+const applyMiddleware=redux.applyMiddleware;
+
+
+const reduxLogger = require('redux-logger');
+const logger = reduxLogger.createLogger();
 
 const CAKE_ORDERED= 'CAKE_ORDERED';
 const CAKE_RESTOCKED='CAKE_RESTOCKED';
@@ -91,7 +96,7 @@ const cakeReducer = (state = initialState, action) => {
   })
   
 // const store =createStore(reducer);
-const store =createStore(rootReducer);
+const store =createStore(rootReducer,applyMiddleware(logger));
 console.log("initial State", store.getState());
 const unSubscribe=store.subscribe(()=>console.log('update state', store.getState()));
 // store.dispatch(orderCake());
